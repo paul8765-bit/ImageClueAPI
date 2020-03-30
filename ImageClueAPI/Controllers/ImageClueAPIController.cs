b7eb36abee153e13e46868022844b152091b9ebd
paul8765-bit <55503865+paul8765-bit@users.noreply.github.com>
2020-03-30 11:56:26 +0100

    Add API and unit tests on API

---
+++ ImageClueAPI/Controllers/ImageClueAPIController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using PairAndImagesLibrary;
+
+namespace ImageClueAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ImageClueAPIController : ControllerBase
+    {
+        private readonly ILogger<ImageClueAPIController> _logger;
+
+        public ImageClueAPIController(ILogger<ImageClueAPIController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            List<string> output = new List<string>();
+            output.Add("Successfully connected to Get API");
+            return output;
+        }
+
+        [HttpGet("getteams/{playerlist}")]
+        public ActionResult<string> GetTeams(string playerlist)
+        {
+            string[] playerlistArray = playerlist.Split("|");
+            return JsonConvert.SerializeObject(PairAndImagesLibraryMain.GetTeams(playerlistArray.ToList()));
+        }
+
+        [HttpGet("getclues/{teamslist}")]
+        public ActionResult<string> GetClues(string teamslist)
+        {
+            List<List<string>> teams = JsonConvert.DeserializeObject<List<List<string>>>(teamslist);
+            return JsonConvert.SerializeObject(PairAndImagesLibraryMain.GetClues(teams));
+        }
+
+        [HttpGet("getteams/verbose/{playerlist}")]
+        public ActionResult<string> GetTeamsVerbose(string playerlist)
+        {
+            StringBuilder output = new StringBuilder();
+            string[] playerlistArray = playerlist.Split("|");
+            List<List<string>> teams = PairAndImagesLibraryMain.GetTeams(playerlistArray.ToList());
+            output.Append(string.Format("Successfully allocated players across {0} teams", teams.Count));
+            for (int teamIndex = 0; teamIndex < teams.Count; teamIndex++)
+            {
+                List<string> currentTeam = teams[teamIndex];
+                output.Append(string.Format("{0} Team {1} has {2} people",
+                    Environment.NewLine, teamIndex + 1, currentTeam.Count));
+                foreach (string currentTeamMember in currentTeam)
+                {
+                    output.Append(string.Format("{0}    {1}", Environment.NewLine, currentTeamMember));
+                }
+            }
+            return output.ToString();
+        }
+    }
+}
---
+++ ImageClueAPITest/ImageClueAPIUnitTest.cs
@@ -0,0 +1,39 @@
+using ImageClueAPI.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using PairAndImagesLibrary;
+using System.Collections.Generic;
+
+namespace ImageClueAPITest
+{
+    [TestClass]
+    public class ImageClueAPIUnitTest
+    {
+        [TestMethod]
+        public void TestGetTeams()
+        {
+            string team1 = "paul|chris|emily|ben";
+            ActionResult<string> result = new ImageClueAPIController(null).GetTeams(team1);
+            string teamsString = result.Value;
+            List<List<string>> teams = JsonConvert.DeserializeObject<List<List<string>>>(teamsString);
+            Assert.AreEqual(2, teams.Count);
+            Assert.AreEqual(2, teams[0].Count);
+            Assert.AreEqual(2, teams[1].Count);
+        }
+
+        [TestMethod]
+        public void TestGetClues()
+        {
+            string team1Json = "[[\"paul\",\"emily\"],[\"chris\",\"ben\"]]";
+            ActionResult<string> result = new ImageClueAPIController(null).GetClues(team1Json);
+            string cluesString = result.Value;
+            List<Clue> clues = JsonConvert.DeserializeObject<List<Clue>>(cluesString);
+            Assert.AreEqual(2, clues.Count);
+
+            // Asert that the nouns are the same, but the adjectives are different
+            Assert.IsTrue(clues[0].Noun == clues[1].Noun);
+            Assert.IsTrue(clues[0].Adjective != clues[1].Adjective);
+        }
+    }
+}
