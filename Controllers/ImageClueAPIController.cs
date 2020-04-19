ae2fc98a761ec027f212e0ca11ab2d9bb7590716
paul8765-bit <55503865+paul8765-bit@users.noreply.github.com>
2020-04-19 10:38:26 +0100

    Remove GetTeamsVerbose unused method

--- Controllers/ImageClueAPIController.cs
+++ Controllers/ImageClueAPIController.cs
@@ -58,34 +58,6 @@ namespace ImageClueAPI.Controllers
             }
         }
 
-        [HttpGet("getteams/verbose/{playerlist}")]
-        public ActionResult<string> GetTeamsVerbose(string playerlist)
-        {
-            try
-            {
-                StringBuilder output = new StringBuilder();
-                List<Tuple<string, string>> playersAndPhoneNumbers =
-                    JsonConvert.DeserializeObject<List<Tuple<string, string>>>(playerlist);
-                List<List<Tuple<string, string>>> teams = PairAndImagesLibraryMain.GetTeams(playersAndPhoneNumbers);
-                output.Append(string.Format("Successfully allocated players across {0} teams", teams.Count));
-                for (int teamIndex = 0; teamIndex < teams.Count; teamIndex++)
-                {
-                    List<Tuple<string, string>> currentTeam = teams[teamIndex];
-                    output.Append(string.Format("{0} Team {1} has {2} people",
-                        Environment.NewLine, teamIndex + 1, currentTeam.Count));
-                    foreach (Tuple<string, string> currentTeamMember in currentTeam)
-                    {
-                        output.Append(string.Format("{0}    {1}", Environment.NewLine, currentTeamMember.Item1));
-                    }
-                }
-                return output.ToString();
-            }
-            catch (Exception e)
-            {
-                return e.Message;
-            }
-        }
-
         [HttpGet("sendsms/{teamslistandclues}")]
         public ActionResult<string> SendSMS(string teamslistandclues)
         {
